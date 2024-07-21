16cbe92e43b1d64a1ac01c4ed5b9006dd9a32015
fano2458 <fano2458@gmail.com>
2024-07-21 17:02:30 +0500

    c++ CNN model train

--- main.cpp
+++ main.cpp
@@ -1,76 +1,132 @@
 #include <torch/torch.h>
 
 
-struct Net: torch::nn::Module {
-    Net() {
-        conv1_1 = register_module("conv1_1", torch::nn::Conv2d(torch::nn::Conv2dOptions(1, 10, 3).padding(1)));
-        conv1_2 = register_module("conv1_2", torch::nn::Conv2d(torch::nn::Conv2dOptions(10, 20, 3).padding(1)));
-        
-        conv2_1 = register_module("conv2_1", torch::nn::Conv2d(torch::nn::Conv2dOptions(20, 30, 3).padding(1)));
-        conv2_2 = register_module("conv2_2", torch::nn::Conv2d(torch::nn::Conv2dOptions(30, 40, 3).padding(1)));
-        
-        conv3_1 = register_module("conv3_1", torch::nn::Conv2d(torch::nn::Conv2dOptions(40, 50, 3).padding(1)));
-        conv3_2 = register_module("conv3_2", torch::nn::Conv2d(torch::nn::Conv2dOptions(50, 60, 3).padding(1)));
-        conv3_3 = register_module("conv3_3", torch::nn::Conv2d(torch::nn::Conv2dOptions(60, 70, 3).padding(1)));
-        
-        conv4_1 = register_module("conv4_1", torch::nn::Conv2d(torch::nn::Conv2dOptions(70, 80, 3).padding(1)));
-        conv4_2 = register_module("conv4_2", torch::nn::Conv2d(torch::nn::Conv2dOptions(80, 90, 3).padding(1)));
-        conv4_3 = register_module("conv4_3", torch::nn::Conv2d(torch::nn::Conv2dOptions(90, 100, 3).padding(1)));
-        
-        conv5_1 = register_module("conv5_1", torch::nn::Conv2d(torch::nn::Conv2dOptions(100, 110, 3).padding(1)));
-        conv5_2 = register_module("conv5_2", torch::nn::Conv2d(torch::nn::Conv2dOptions(110, 120, 3).padding(1)));
-        conv5_3 = register_module("conv5_3", torch::nn::Conv2d(torch::nn::Conv2dOptions(120, 130, 3).padding(1)));
-        
-        fc1 = register_module("fc1", torch::nn::Linear(130, 50));
-        fc2 = register_module("fc2", torch::nn::Linear(50, 20));
-        fc3 = register_module("fc3", torch::nn::Linear(20, 10));
-    }
+const char* kDataRoot = "./data";
+const int64_t kTrainBatchSize = 64;
+const int64_t kTestBatchSize = 1000;
+const int64_t kNumberOfEpochs = 15;
+const int64_t kLogInterval = 10;
 
-    torch::Tensor forward(torch::Tensor x) {
-        x = torch.relu(conv1_1->forward(x));
-        x = torch.relu(conv1_2->forward(x));
-        x = torch::max_pool2d(x, 2);
+struct Net : torch::nn::Module {
+  Net()
+	  : conv1(torch::nn::Conv2dOptions(1, 10, 5)),
+		conv2(torch::nn::Conv2dOptions(10, 20, 5)),
+        conv3(torch::nn::Conv2dOptions(20, 30, 3)),
+		fc1(120, 50),
+		fc2(50, 10) {
+	register_module("conv1", conv1);
+	register_module("conv2", conv2);
+    register_module("conv3", conv3);
+	register_module("fc1", fc1);
+	register_module("fc2", fc2);
+  }
 
-        x = torch.relu(conv2_1->forward(x));
-        x = torch.relu(conv2_2->forward(x));
-        x = torch::max_pool2d(x, 2);
+  torch::Tensor forward(torch::Tensor x) {
+	x = torch::gelu(torch::max_pool2d(conv1->forward(x), 2));
+	x = torch::gelu(torch::max_pool2d(conv2->forward(x), 2));
+    // std::cout << x.sizes() << std::endl;
+    x = torch::gelu(conv3->forward(x));
+    // std::cout << x.sizes() << std::endl;
+	x = x.view({-1, 120});
+	x = torch::gelu(fc1->forward(x));
+	x = torch::dropout(x, 0.5, is_training());
+	x = fc2->forward(x);
+	return torch::log_softmax(x, 1);
+  }
 
-        x = torch::relu(conv3_1->forward(x));
-        x = torch::relu(conv3_2->forward(x));
-        x = torch::relu(conv3_3->forward(x));
-        x = torch::max_pool2d(x, 2);
-
-        x = torch::relu(conv4_1->forward(x));
-        x = torch::relu(conv4_2->forward(x));
-        x = torch::relu(conv4_3->forward(x));
-        x = torch::max_pool2d(x, 2);
-
-        x = torch::relu(conv5_1->forward(x));
-        x = torch::relu(conv5_2->forward(x));
-        x = torch::relu(conv5_3->forward(x));
-
-        x = x.view({-1, 130});
-
-        x = torch::relu(fc1->forward(x));
-        x = torch::relu(fc2->forward(x));
-        x = fc3->forward(x);
-
-        return torch::log_softmax(x, 1);
-    }
-
-    torch::nn::Conv2d conv1_1{nullptr};
-    torch::nn::Conv2d conv1_2{nullptr};
-    torch::nn::Conv2d conv2_1{nullptr};
-    torch::nn::Conv2d conv2_2{nullptr};
-    torch::nn::Conv2d conv3_1{nullptr};
-    torch::nn::Conv2d conv3_2{nullptr};
-    torch::nn::Conv2d conv3_3{nullptr};
-    torch::nn::Conv2d conv4_1{nullptr};
-    torch::nn::Conv2d conv4_2{nullptr};
-    torch::nn::Conv2d conv4_3{nullptr};
-    torch::nn::Conv2d conv5_1{nullptr};
-    torch::nn::Conv2d conv5_2{nullptr};
-    torch::nn::Conv2d conv5_3{nullptr};
-
-    torch::nn::Linear fc1{nullptr}, fc2{nullptr}, fc3{nullptr};
+  torch::nn::Conv2d conv1;
+  torch::nn::Conv2d conv2;
+  torch::nn::Conv2d conv3;
+  torch::nn::Linear fc1;
+  torch::nn::Linear fc2;
 };
+
+template <typename DataLoader>
+void train(size_t epoch, Net& model, torch::Device device, DataLoader& data_loader, 
+		  torch::optim::Optimizer& optimizer, size_t dataset_size) {
+  model.train();
+  size_t batch_idx = 0;
+  for (auto& batch : data_loader) {
+	auto data = batch.data.to(device), targets = batch.target.to(device);
+	optimizer.zero_grad();
+	auto output = model.forward(data);
+	auto loss = torch::nll_loss(output, targets);
+	loss.backward();
+	optimizer.step();
+
+	if (batch_idx++ % kLogInterval == 0) {
+	  std::printf(
+		  "\rTrain Epoch: %ld [%5ld/%5ld] Loss: %.4f",
+		  epoch,
+		  batch_idx * batch.data.size(0),
+		  dataset_size,
+		  loss.template item<float>());
+	}
+  }
+}
+
+template <typename DataLoader>
+void test(Net& model, torch::Device device, DataLoader& data_loader, size_t dataset_size) {
+  torch::NoGradGuard no_grad;
+  model.eval();
+  double test_loss = 0;
+  int32_t correct = 0;
+  for (const auto& batch : data_loader) {
+	auto data = batch.data.to(device), targets = batch.target.to(device);
+	auto output = model.forward(data);
+	test_loss += torch::nll_loss(
+					 output,
+					 targets,
+					 /*weight=*/{},
+					 torch::Reduction::Sum)
+					 .template item<float>();
+	auto pred = output.argmax(1);
+	correct += pred.eq(targets).sum().template item<int64_t>();
+  }
+
+  test_loss /= dataset_size;
+  std::printf(
+	  "\nTest set: Average loss: %.4f | Accuracy: %.3f\n",
+	  test_loss,
+	  static_cast<double>(correct) / dataset_size);
+}
+
+auto main() -> int {
+  torch::manual_seed(1);
+
+  torch::DeviceType device_type;
+  if (torch::cuda::is_available()) {
+	std::cout << "CUDA available! Training on GPU." << std::endl;
+	device_type = torch::kCUDA;
+  } else {
+	std::cout << "Training on CPU." << std::endl;
+	device_type = torch::kCPU;
+  }
+  torch::Device device(device_type);
+
+  Net model;
+  model.to(device);
+
+  auto train_dataset = torch::data::datasets::MNIST(kDataRoot)
+						   .map(torch::data::transforms::Normalize<>(0.1307, 0.3081))
+						   .map(torch::data::transforms::Stack<>());
+						   
+  const size_t train_dataset_size = train_dataset.size().value();
+  auto train_loader = torch::data::make_data_loader<torch::data::samplers::SequentialSampler>(
+		  std::move(train_dataset), kTrainBatchSize);
+
+  auto test_dataset = torch::data::datasets::MNIST(
+						  kDataRoot, torch::data::datasets::MNIST::Mode::kTest)
+						  .map(torch::data::transforms::Normalize<>(0.1307, 0.3081))
+						  .map(torch::data::transforms::Stack<>());
+
+  const size_t test_dataset_size = test_dataset.size().value();
+  auto test_loader = torch::data::make_data_loader(std::move(test_dataset), kTestBatchSize);
+
+  torch::optim::SGD optimizer(model.parameters(), torch::optim::SGDOptions(0.01).momentum(0.5));
+
+  for (size_t epoch = 1; epoch <= kNumberOfEpochs; ++epoch) {
+	train(epoch, model, device, *train_loader, optimizer, train_dataset_size);
+	test(model, device, *test_loader, test_dataset_size);
+  }
+}
